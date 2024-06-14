bb9e2f3fc4c8d3520c90d493924e83834bf9d88d
DevBobcorn <devbobcorn@outlook.com>
2024-06-14 11:04:05 +0800

    :bug: Fix NeoForge config screen register

--- neoforge/src/main/java/io/devbobcorn/acrylic/forge/AcrylicModNeoForge.java
+++ neoforge/src/main/java/io/devbobcorn/acrylic/forge/AcrylicModNeoForge.java
@@ -1,5 +1,6 @@
 package io.devbobcorn.acrylic.forge;
 
+import com.mojang.logging.LogUtils;
 import io.devbobcorn.acrylic.AcrylicMod;
 import io.devbobcorn.acrylic.client.screen.ConfigScreenUtil;
 import net.minecraft.client.Minecraft;
@@ -9,26 +10,33 @@ import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
 
 @Mod(AcrylicMod.MOD_ID)
-public class AcrylicModForge {
+public class AcrylicModNeoForge {
 
-    public AcrylicModForge(IEventBus bus) {
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public AcrylicModNeoForge(IEventBus bus) {
         bus.addListener(this::clientSetup);
     }
 
-    class AcrylicConfigScreenFactory implements IConfigScreenFactory {
+    static class AcrylicConfigScreenFactory implements IConfigScreenFactory {
 
         @Override
-        public Screen createScreen(Minecraft mc, Screen screen) {
+        public @NotNull Screen createScreen(@NotNull Minecraft mc, @NotNull Screen screen) {
             return ConfigScreenUtil.create(screen);
         }
     }
 
     private void clientSetup(final FMLCommonSetupEvent event) {
 
-        ModLoadingContext.get().registerExtensionPoint(AcrylicConfigScreenFactory.class,
+        ModLoadingContext.get().registerExtensionPoint(
+                IConfigScreenFactory.class,
                 AcrylicConfigScreenFactory::new);
+
+        LOGGER.info("Config screen registered.");
 
     }
 }
